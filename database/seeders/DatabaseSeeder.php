2de4a3f53c0b3ca27f36267d25216d6fe9729301
Mauricio Duran <mdurant@MacBook-Air-Mauricio.local>
2025-06-29 13:51:32 -0400

    add lang:es

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,21 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            UsersSeeder::class,
+            BanksSeeder::class,
+            BanksAccountTypesSeeder::class,
+            BusinessActivitiesSeeder::class,
+            CitiesSeeder::class,
+            CommunesSeeder::class,
+            JobTitlesSeeder::class,
+            AccountingAccountsSeeder::class,
+            PriceListsSeeder::class,
+            PaymentConditionsSeeder::class,
+            PurchaseConceptsSeeder::class,
+            BusinessUnitsSeeder::class,
+            CompaniesSeeder::class,
+            PurchaseTypeSeeder::class,
         ]);
     }
 }
